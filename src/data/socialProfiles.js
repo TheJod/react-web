d6eceff2a4c33e48ecb955d3350e5a2bf9edac28
Jod Kasidit <kasidit.puengpanich@agoda.com>
2021-06-29 22:00:40 +0700

    added icon assets and data js

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,23 @@
+import emailIcon from '../Assets/email-icon-3.png';
+import twitterIcon from '../Assets/twitter-icon-4.png';
+import facebookIcon from '../Assets/facebook-icon-2.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1, 
+        link: 'mailto:info@lattenomic.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://www.twitter.com',
+        image: twitterIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.facebook.com',
+        image: facebookIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
